f5e2b7bdbb401cc58cfaa799788c3577ae33247a
Zhi Liu <zhiliu.git@gmail.com>
2018-12-06 12:55:56 -0500

    NN Keras pima diabetes code

---
+++ Neural_Network_Keras.py
@@ -0,0 +1,53 @@
+"""
+Full tutorial: https://machinelearningmastery.com/tutorial-first-neural-network-python-keras/
+"""
+
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy
+# fix random seed for reproducibility
+numpy.random.seed(7)
+
+
+# load pima indians dataset
+
+dataset = numpy.loadtxt("https://raw.githubusercontent.com/zhiliu-git/Datasets/master/pima-indians-diabetes.data.csv", delimiter=",")
+# split into input (X) and output (Y) variables
+X = dataset[:,0:8]
+Y = dataset[:,8]
+
+# create model
+model = Sequential()
+model.add(Dense(12, input_dim=8, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# Compile model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Fit the model
+model.fit(X, Y, epochs=150, batch_size=10)
+
+# evaluate the model
+scores = model.evaluate(X, Y)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+######### predictions -- using the same dataset. partially repeat of previous codes ############
+
+model = Sequential()
+model.add(Dense(12, input_dim=8, init='uniform', activation='relu'))
+
+model.add(Dense(8,  init='uniform', activation='relu'))
+model.add(Dense(1,  init='uniform', activation='sigmoid'))
+
+# Compile model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Fit the model
+model.fit(X, Y, epochs=150, batch_size=10, verbose =2)
+
+# calculate predictions
+predictions = model.predict(X)
+# round predictions
+rounded = [round(x[0]) for x in predictions]
+print(rounded)
